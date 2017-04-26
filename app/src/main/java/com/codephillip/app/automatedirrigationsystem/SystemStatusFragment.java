ce4275df8985615c036f96bc593097e7c02d9ba1
codephillip <codephillip@gmail.com>
2017-04-26 15:23:40 +0300

    added irrigation status, fab -> Chart

--- app/src/main/java/com/codephillip/app/automatedirrigationsystem/SystemStatusFragment.java
+++ app/src/main/java/com/codephillip/app/automatedirrigationsystem/SystemStatusFragment.java
@@ -1,8 +1,10 @@
 package com.codephillip.app.automatedirrigationsystem;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
@@ -12,15 +14,18 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.codephillip.app.automatedirrigationsystem.adapters.MetricAdapter;
 import com.codephillip.app.automatedirrigationsystem.provider.metrictable.MetrictableColumns;
+import com.codephillip.app.automatedirrigationsystem.provider.metrictable.MetrictableCursor;
 
 
 public class SystemStatusFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
 
     MetricAdapter adapter;
     RecyclerView recyclerView;
+    TextView irrigationgView;
 
     public SystemStatusFragment() {
     }
@@ -29,6 +34,14 @@ public class SystemStatusFragment extends Fragment implements LoaderManager.Load
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_system_status, container, false);
+        irrigationgView = (TextView) rootView.findViewById(R.id.irrigating);
+        FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), LineChartActivity.class));
+            }
+        });
         recyclerView = (RecyclerView) rootView.findViewById(R.id.status_recycler);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -51,6 +64,13 @@ public class SystemStatusFragment extends Fragment implements LoaderManager.Load
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         adapter.swapCursor(data);
+        populateIrrigationViewData(data);
+    }
+
+    private void populateIrrigationViewData(Cursor data) {
+        data.moveToFirst();
+        MetrictableCursor metric = new MetrictableCursor(data);
+        irrigationgView.setText("IRRIGATION: " + metric.getIsirrigating());
     }
 
     @Override
